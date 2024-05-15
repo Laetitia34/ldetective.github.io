553a69f32e3f2fe36bc0ff2721bb9a76b74b8806
Laetitia34 <titirom23@hotmail.fr>
2024-05-15 18:41:39 +0200

    php

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -18,11 +18,11 @@
   // Replace with your real receiving email address
   $contactform->to = 'contact@example.com';
   $contactform->from_name = $_POST['name'];
-  $contactform->from_email = $_POST['ldetective7@proton.me'];
+  $contactform->from_email = $_POST['email'];
   $contactform->subject = $_POST['subject'];
 
   $contactform->add_message( $_POST['name'], 'From');
-  $contactform->add_message( $_POST['email'], 'Email');
+  $contactform->add_message( $_POST['ldetective7@proton.me'], 'ldetective7@proton.me');
   $contactform->add_message( $_POST['message'], 'Message', 10);
 
   echo $contactform->send();
